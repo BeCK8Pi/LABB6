bea9d94cbcb34470db3a887110485328595f78ca
BeCK8Pi <khuntip1207@gmail.com>
2023-12-01 15:47:47 +0700

    add everything

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int n, even = 0, odd = 0;
+    while(n != 0){
+        cout << "Enter an integer: ";
+        cin >> n;
+        if(n != 0){
+            if(n%2 == 0){
+                even = even +1;
+            }
+            else{
+                odd = odd +1;
+            }
+        }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
